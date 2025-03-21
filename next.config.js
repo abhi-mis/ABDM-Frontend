ad004eba715a7de6149ff9efe19eb434d48b0686
abhi-mis <abpolar790@gmail.com>
2025-03-21 21:45:55 +0530

    built

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    domains: ['upload.wikimedia.org'], // Add the domain here
-  },
+  trailingSlash: true,  // Ensure correct static file paths
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true, domains: ['upload.wikimedia.org'] },
   reactStrictMode: true,
 };
 
