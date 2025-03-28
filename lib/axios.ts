1f2f66dda9509042e1257b7b5be82aabc7d13a63
Nishka30 <shrimalinishka@gmail.com>
2025-03-28 13:27:26 +0530

    fix:token error

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'apiabdm.docbot.in';
 
 export const apiClient = axios.create({
-  baseURL: `https://${BASE_URL}`,
+  baseURL: `${BASE_URL}`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
